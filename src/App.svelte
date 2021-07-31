dc1e7008ac69e43a28af6bea90f1f11614a8e34c
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-07-31 14:13:41 +0545

    Removed default export

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Box, { Column, Row } from "./index.js";
+	import { Box, Column, Row } from "./index.js";
 </script>
 
 <Column>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as Box } from "./Skeleton/Box.svelte";
 export { default as Column } from "./Skeleton/Column.svelte";
 export { default as Row } from "./Skeleton/Row.svelte";
-export { default } from "./Skeleton/Box.svelte";
